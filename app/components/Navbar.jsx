9bd9dbdc35d01add53c9da75b433429ffe05c7b0
Homayoun <homayoun763@gmail.com>
2024-03-25 17:52:31 +0330

    navbar

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import SiteConfig from "../config/site";
+import Link from "next/link";
+
+const Navbar = () => {
+  const nav = SiteConfig.nav;
+  return (
+    <div>
+      <div className="fixed top-0 left-0 right-0 navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {nav.map((item) => (
+                <li key={item.id}>
+                  <Link href={item.link}>{item.name}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl">{SiteConfig.name}</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            {nav.map((item) => (
+              <li key={item.id}>
+                <Link href={item.link}>{item.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <div className="dropdown ">
+            <div tabIndex={0} role="button" className="btn m-1">
+              تم
+              <svg
+                width="12px"
+                height="12px"
+                className="h-2 w-2 fill-current opacity-60 inline-block"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 2048 2048"
+              >
+                <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="absolute left-1 dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52"
+            >
+              <li>
+                <input
+                  type="radio"
+                  name="theme-dropdown"
+                  className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                  aria-label="cupcake"
+                  value="cupcake"
+                />
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="theme-dropdown"
+                  className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                  aria-label="retro"
+                  value="retro"
+                />
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="theme-dropdown"
+                  className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                  aria-label="nord"
+                  value="nord"
+                />
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="theme-dropdown"
+                  className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                  aria-label="Aqua"
+                  value="aqua"
+                />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- app/config/site.js
+++ app/config/site.js
@@ -4,6 +4,23 @@ const SiteConfig = {
   description: "شما بدون وب سایت هیچ هویتی ندارید",
   lang: "fa",
   dir: "rtl",
+  nav :[
+    {
+      id : 1 ,
+      name : "خانه",
+      link : "/"
+    },
+    {
+      id : 2 ,
+      name : "تماس با ما",
+      link : "/"
+    },
+    {
+      id : 3 ,
+      name : "درباره ما",
+      link : "/"
+    },
+  ]
 };
 
 export default SiteConfig;
