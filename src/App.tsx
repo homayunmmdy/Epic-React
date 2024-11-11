d25088e2f24589ca89f3aeb2068ab4183cacdd2f
Homayoun <homayoun763@gmail.com>
2024-11-11 23:34:27 +0330

    update the project and type for calculator props

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+
+
+const operations = {
+  "+": (left: number, right: number): number => left + right,
+  "-": (left: number, right: number): number => left - right,
+  "*": (left: number, right: number): number => left * right,
+  "/": (left: number, right: number): number => left / right,
+};
+
+type CalculatorProps = {
+  left: number;
+  operator: string;
+  right: number;
+};
+
+function Calculator({ left, operator, right }: CalculatorProps) {
+  // @ts-expect-error we'll fix this one later
+  const result = operations[operator](left, right);
+  return (
+    <div>
+      <code>
+        {left} {operator} {right} = <output>{result}</output>
+      </code>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Calculator</h1>
+      <Calculator left={1} operator="+" right={2} />
+      <Calculator left={1} operator="-" right={2} />
+      <Calculator left={1} operator="*" right={2} />
+      <Calculator left={1} operator="/" right={2} />
+    </div>
+  );
+}
+
+export default App;
