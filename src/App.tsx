b912cead1abf40c851a34814bad5ce88215f6b3b
Homayoun <homayoun763@gmail.com>
2024-11-12 18:24:30 +0330

    send action with form

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,11 @@
-import "./app.css";
-
-function Box({
-	style = {},
-	className = '',
-	size,
-	...otherProps
-}: {
-	size?: 'small' | 'medium' | 'large'
-} & React.HTMLAttributes<HTMLDivElement>) {
-	const sizeClassName = size ? `box--${size}` : ''
-	return (
-		<div
-			className={`box ${className} ${sizeClassName}`}
-			style={{ fontStyle: 'italic', ...style }}
-			{...otherProps}
-		/>
-	)
-}
-
-function App() {
+const App = () => {
   return (
-    <div>
-    <Box size="small" style={{ backgroundColor: 'lightblue' }}>
-      small lightblue box
-    </Box>
-    <Box size="medium" style={{ backgroundColor: 'pink' }}>
-      medium pink box
-    </Box>
-    <Box size="large" style={{ backgroundColor: 'orange' }}>
-      large orange box
-    </Box>
-    <Box>sizeless colorless box</Box>
-  </div>
+    <form action="/api/login">
+      <label htmlFor="username">UserName</label>
+      <input id="username" name="username"/>
+      <button type="submit">Submit</button>
+    </form>
   );
-}
+};
 
 export default App;
