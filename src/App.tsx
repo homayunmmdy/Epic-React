2d4976732379aaf3281a08221038561cc7c74946
Homayoun <homayoun763@gmail.com>
2024-11-12 18:33:04 +0330

    input types in the form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 const App = () => {
   return (
     <form action="/api/login">
-      <label htmlFor="username">UserName</label>
-      <input id="username" name="username"/>
+      <fieldset>
+        <label htmlFor="username">UserName</label>
+        <input id="username" name="username" type="text" />
+      </fieldset>
+      <fieldset>
+        <label htmlFor="password">password</label>
+        <input id="password" name="password" type="password" />
+      </fieldset>
+      <fieldset>
+        <label htmlFor="age">age</label>
+        <input id="age" name="age" type="number" />
+      </fieldset>
+      <fieldset>
+        <label htmlFor="photo">photo</label>
+        <input id="photo" type="file" name="photo"  accept="image/*"/>
+      </fieldset>
+      <fieldset>
+        <label htmlFor="color">Favorite Color</label>
+        <input id="color" type="color" name="color" />
+      </fieldset>
+      <fieldset>
+        <label htmlFor="date">Start Date</label>
+        <input id="date" type="date" name="date" />
+      </fieldset>
       <button type="submit">Submit</button>
     </form>
   );
