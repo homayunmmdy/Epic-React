339d0225d84da26ccd90d206286ae1e5817d0f5a
Homayoun <homayoun763@gmail.com>
2024-11-13 18:25:21 +0330

    reset error

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import { createRoot } from "react-dom/client";
 import { ErrorBoundary, FallbackProps } from "react-error-boundary";
 import App from "./App.tsx";
 
-function ErrorFallback({ error }: FallbackProps) {
+function ErrorFallback({ error, resetErrorBoundary }: FallbackProps) {
   return (
     <div role="alert">
       There was an error:{" "}
       <pre style={{ color: "red", whiteSpace: "normal" }}>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
     </div>
   );
 }
