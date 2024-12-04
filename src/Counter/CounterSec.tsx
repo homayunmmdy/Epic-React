eda619afb036ae639e6fcece0253111a123126a7
Homayoun <homayoun763@gmail.com>
2024-12-04 22:23:40 +0330

    tradition way of reducer

--- src/Counter/CounterSec.tsx
+++ src/Counter/CounterSec.tsx
@@ -2,21 +2,38 @@ import { useReducer, useState } from "react";
 import "./style.css";
 
 type State = { count: number };
-type Action = Partial<State> | ((state: State) => Partial<State>);
-const countReducer = (state: State, action: Action) => ({
-  ...state,
-  ...(typeof action === "function" ? action(state) : action),
-});
+type Action =
+  | { type: "increment"; step: number }
+  | { type: "decrement"; step: number };
+  function countReducer(state: State, action: Action) {
+    const { type, step } = action
+    switch (type) {
+      case 'increment': {
+        return {
+          ...state,
+          count: state.count + step,
+        }
+      }
+      case 'decrement': {
+        return {
+          ...state,
+          count: state.count - step,
+        }
+      }
+    }
+  }
 
 function Counter({ initialCount = 0, step = 1 }) {
-  const [state, setState] = useReducer(countReducer, {
+  const [state, dispatch] = useReducer(countReducer, {
     count: initialCount,
   });
   const { count } = state;
+  // 🐨 the logic has now been moved back to the reducer, update these to pass
+  // the appropriate action object to the dispatch function
   const increment = () =>
-    setState((currentSate) => ({ count: currentSate.count + step }));
+    dispatch({type : 'increment', step});
   const decrement = () =>
-    setState((currentSate) => ({ count: currentSate.count - step }));
+    dispatch({type : 'decrement', step});
   return (
     <div className="counter">
       <output>{count}</output>
